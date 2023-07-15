1934f1fd07de5eff8915a4dec4e64f390c0ef857
love2007-web <adeoluwaloveowolabi@gmail.com>
2023-07-15 15:23:24 +0100

    first commit

---
+++ frontend/src/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className=''>
+        <h1 className=''>hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
