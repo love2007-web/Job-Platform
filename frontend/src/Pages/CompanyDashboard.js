4b5aafcd22745d47a35a70b4f759bd384cc60b59
love2007-web <adeoluwaloveowolabi@gmail.com>
2023-07-31 12:42:22 +0100

    .

--- frontend/src/Pages/CompanyDashboard.js
+++ frontend/src/Pages/CompanyDashboard.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import CompanyDashboardSidebar from './CompanyDashboardSidebar'
 
 const CompanyDashboard = () => {
   return (
     <>
-        
+        <CompanyDashboardSidebar/>
     </>
   )
 }
---
+++ frontend/src/Pages/CompanyDashboardSidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CompanyDashboardSidebar = () => {
+  return (
+    <>
+        <h3 className='bg-red-600'>kjsdflasdf</h3>
+    </>
+  )
+}
+
+export default CompanyDashboardSidebar
